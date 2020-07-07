c618393c06f49d5a992bae7111f124ae30149774
sahashiharshit <harshitsah96@gmail.com>
2020-07-07 20:44:01 +0530

    adding navbar

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-const express = require('express');
+const express = require("express");
+
+
+const path = require("path");
 //express app
 const app = express();
+
+// const router = express.Router();
 const PORT = process.env.PORT || 3000;
-//Listening for requests
+
+
+// Listening for requests
 app.listen(PORT);
 
-app.get('/', (req, res) => {
-    
-    res.send("<p>Construction work in progress.</p>");
+
+// set the views 
+app.set("views", path.join(__dirname, "views"));
+//set the view engine
+app.set("view engine", "pug");
+//index page
+app.get("/", (req, res) => {
+  res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+  res.render('home', {
+    title: "Home",
+  });
 });
+
+app.get("/portfolio", (req, res) =>{
+res.render('portfolio',{
+  title:"Portfolio",
+});
+});
+//about WebPage
+app.get("/about", (req, res) => {
+  res.render('about', {
+    title: "About Me",
+  });
+});
+//404 page
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+// module.exports = router
